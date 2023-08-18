89e19721d45637fad82e3408a5e3410a723768bf
Kenneth Ofosu <kennethofosu01@gmail.com>
2023-08-18 19:32:43 -0400

    feat(api): data fetching operational

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+require('dotenv').config()
+const https = require('https')
+
 const cityName = process.argv[2]
 console.log(cityName)
+console.log(process.env.API_KEY)
+
+https.get(`${process.env.BASE_URL}${cityName}?key=${process.env.API_KEY}`, (response) => {
+    let data = ''
+
+    response.on('data', (chunk) => {
+        data += chunk
+    })
+
+    response.on('end', () => {
+        console.log(JSON.parse(data))
+    })   
+}).on('error', (err) => {
+    console.log(err)
+})
